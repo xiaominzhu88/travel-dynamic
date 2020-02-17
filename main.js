7b1ae2939aa61a0617e96c227e967b50f79610f5
xiaominzhu88 <xiaomin.zhu88@gmail.com>
2020-02-17 22:46:27 +0100

    new file

---
+++ main.js
@@ -0,0 +1,334 @@
+const results = {
+  data: [
+    {
+      name: "china",
+      image:
+        "https://www.welt.de/img/wirtschaft/mobile172151879/2642507627-ci102l-w1024/Shanghai-Sunrise.jpg",
+      types: ["asien"],
+      aboutText:
+        "China ist flächenmäßig das zweitgrößte Land der Welt. Das Wetter spielt für Reisende immer eine große Rolle. Wegen der enormen Größe Chinas gibt es im Land große Unterschiede. In der nordöstlichen Stadt Harbin hat es im Winter rund -30°C. Shanghai im Sommer kann im Vergleich mit 35°C im Sommer zu Sauna werden,die Hauptreligionen sind der Buddhismus, der Taoismus, der Islam, der Katholizismus und das Christentum. Hochchenisisch gild als Amtssprache, jedoch gibt es einige autonome Gebiete in denen zahlreiche Dialekte gesprochen werden. Englisch wird in gebildeteren Gegenden so gut wie immer verstanden. \
+        Chinas Wirtschaft ist weiterhin im Wachstum und hat Kurs auf die weltweit größte Volkswirtschaft. Das Land ist außerdem sehr bekannt wegen seinem kunstvollen Porzellan, der eindrucksvollen Oper in Peking, der feinen Seide, der Kampfkunst, der chinesischen Medizin und den leckeren Mehlspeisen. Tischtennis, Go, Mahjongg und Fünf in einer Reihe sind in China populäre Spiele.\
+        Zahlen und Farben haben in China eine große Bedeutung, so werden gerade Zahlen ungeraden bevorzugt. Die Zahl 8 steht für Wohlstand, Erfolg und sozialen Status an. Die Farbe Gelb steht für Königtum und Macht. Rot steht für Glück, Erfolg und Schönheit und Grün steht für Geld.",
+
+      link:
+        "https://www.google.com/maps/d/u/0/viewer?msa=0&mid=1IdNutKg3GDszFXFeD9-0l4iUiJk&ll=32.07192158699564%2C106.29628449999996&z=5"
+    },
+
+    {
+      name: "singapur",
+      image:
+        "https://www.auslandslust.de/wp-content/uploads/xauswandern-singapur-696x293.jpg.pagespeed.ic.QHwrY1OOgj.jpg",
+      types: ["asien"],
+      aboutText:
+        "Singapur (amtlich Republik Singapur, englisch Republic of Singapore, malaiisch Republik Singapura, chinesisch 新加坡共和国, Pinyin Xīnjiāpō Gònghéguó, auch: 新加坡 [ɕin.tɕiɑ.pʰuɔ], Tamil சிங்கப்பூர் குடியரசு Ciṅkappūr Kudiyarasu) ist ein Insel- und Stadtstaat und der flächenmäßig kleinste Staat Südostasiens. Er ist Mitglied im Commonwealth of Nations,\
+        Beim Index der menschlichen Entwicklung belegte Singapur 2017 den neunten Platz. Singapur ist eines der reichsten Länder (und Städte) weltweit und gilt als eine der Städte mit den weltweit höchsten Lebenshaltungskosten. Zudem zählt der Stadtstaat mit mehr als elf Millionen ausländischen Touristen im Jahr zu den zehn meistbesuchten Städten der Welt und gilt neben Hongkong als wichtigster Finanzplatz Asiens. Singapur ist ein multiethnischer Staat, in dem Chinesen, Malaien und Inder die größten Bevölkerungsteile stellen",
+      link:
+        "https://www.google.com/maps/d/u/0/viewer?ie=UTF&msa=0&mid=1Xbkrh789WSsHPBfq4_JpXs5EV2g&ll=1.301189000000015%2C103.858609&z=17"
+    },
+
+    {
+      name: "thailand",
+      image:
+        "https://www.urlaubstracker.at/wp-content/uploads/2019/08/thailand-khao-sok-nationalpark.jpg",
+      types: ["asien"],
+      aboutText:
+        "Thailand ist eines beliebtesten Fernreisezielen. Besonders bekannt sind die Strände und Inseln im Süden des Landes, welche sich durch ihren wunderschönen Sand und das türkis-blaue Meer auszeichnen. Die Unterwasserwelt erwartet einen mit farbenfrohen Fischen und Korallen. Thailand bietet viele Möglichkeiten für individual Reisende, durch die gut ausgebaute touristische Infrastruktur, detaillierten Informationen im Internet und Reiseführern kann man die Reise genau nach dem eigenen Belieben geplant werden. ",
+      link:
+        "https://www.google.com/maps/d/u/0/viewer?msa=0&hl=de&ie=UTF8&t=h&ll=12.189704000000006%2C100.78857399999993&spn=\
+        8.58324%2C10.217285&z=7&source=embed&mid=1w2dDfVpIN8_M80H64XiK1Q8yVWo"
+    },
+
+    {
+      name: "japan",
+      image:
+        "https://www.travelbook.de/data/uploads/2018/03/gettyimages-800417474_1521806552-1040x690.jpg",
+      types: ["asien"],
+      aboutText:
+        "Japan (japanisch 日本, ausgesprochen als Nihon oder Nippon; , Japan, Travelguide.at, Travelguide.at anhören?/i) ist ein 6852 Inseln umfassender ostasiatischer Staat im Pazifik, der indirekt im Norden und Nordwesten an Russland, im Westen an Nord- und Südkorea und im Südwesten an Taiwan und China grenzt und flächenmäßig der viertgrößte Inselstaat der Welt ist. De-facto-Hauptstadt und größte urbane Siedlung ist Tokio.\
+         Die Bildung des japanischen Staatswesens begann im 5. Jahrhundert unter kulturellem Einfluss des chinesischen Kaiserreichs. Seit dem 16. Jahrhundert stand Japan im Kontakt mit dem Westen und stieg seit dem 19. Jahrhundert zur Großmacht auf, erwarb Kolonien wie Korea und Taiwan, nahm an beiden Weltkriegen teil und beherrschte kurzzeitig große Teile Südost- und Ostasiens.",
+      link:
+        "https://www.google.com/maps/d/u/0/viewer?ie=UTF8&hl=en&msa=0&ll=35.56560700000001%2C139.52447900000004&spn=\
+        0.967391%2C2.471924&z=9&mid=1qzIqwLbfa1WLiZyXDflPj9yrf00"
+    },
+
+    {
+      name: "germany",
+      image:
+        "https://static01.nyt.com/images/2018/10/24/business/19krugman-web/24italyexplain-1-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+      types: ["europa"],
+      aboutText:
+        "Deutschland (Vollform: Bundesrepublik Deutschland) ist ein Bundesstaat in Mitteleuropa. Er besteht aus 16 Ländern und ist als freiheitlich-demokratischer und sozialer Rechtsstaat verfasst. Die Bundesrepublik Deutschland stellt die jüngste Ausprägung des deutschen Nationalstaates dar. Deutschland hat über 82,5 Millionen Einwohner und zählt mit 232 Einwohnern pro km² zu den dicht besiedelten Flächenstaaten.",
+      link:
+        "https://www.google.com/maps/d/u/0/viewer?mid=1jWnodsZMQI4XRTazLmsrWL6xLYQ&hl=en_US&ll=51.22344270096483%2C10.527648999999997&z=6"
+    },
+
+    {
+      name: "polen",
+      image:
+        "https://www.kurzurlaub.de/img/statisch_teaser/de/teaser_image_2380_teaser-land-polen.jpg",
+      types: ["europa"],
+      aboutText:
+        "Polen (polnisch Polska [ˈpɔlska], amtlich Rzeczpospolita Polska, [ʐɛʈ͡ʂpɔsˈpɔlita ˈpɔlska], deutsch Republik Polen) ist eine parlamentarische Republik in Mitteleuropa. Hauptstadt und zugleich größte Stadt des Landes ist Warschau (polnisch Warszawa), größter Ballungsraum die Metropolregion um Katowice. Polen ist ein in 16 Woiwodschaften gegliederter Einheitsstaat. Mit einer Größe von 312.679 Quadratkilometern ist Polen das sechstgrößte Land der Europäischen Union und mit 38,5 Millionen Einwohnern ebenfalls das sechstbevölkerungsreichste. Es herrscht vorwiegend das ozeanische Klima im Norden und Westen sowie das kontinentale Klima im Süden und Osten des Landes.",
+      link:
+        "https://www.google.com/maps/d/u/0/embed?mid=1c1CS9mOYL26PfScoTDTrTtE5fWk&msa=0&ie=UTF8&t=m&ll=\
+        56.38958399999999%2C17.622070000000008&spn=6.816219%2C21.928711&z=5&output=embed"
+    },
+
+    {
+      name: "spain",
+      image:
+        "https://prodblogsunwebstore.blob.core.windows.net/media/2017/08/spain.jpg",
+      types: ["europa"],
+      aboutText:
+        "Spanien (amtlich Königreich Spanien, spanisch Reino de España [ˈrejno ð(e) esˈpaɲa] , auf Galicisch Reino de España, Katalanisch Regne d’Espanya, Asturisch Reinu d’España, Baskisch Espainiako Erresuma, Aranesisch Reialme d’Espanha, Aragonesisch Reino d’Espanya) ist ein Staat auf der Iberischen Halbinsel im Südwesten Europas mit zwei Exklaven in Nordafrika.\
+        Staatsform ist eine parlamentarische Erbmonarchie. Spanien untergliedert sich in 17 autonome Gemeinschaften und zwei autonome Städte (spanisch ciudades autónomas), Ceuta und Melilla. Die Hauptstadt und größte Metropole ist Madrid, weitere Ballungszentren sind Barcelona, Valencia, Sevilla, Bilbao, Zaragoza und Málaga.",
+      link:
+        "https://www.google.com/maps/d/u/0/viewer?msa=0&hl=de&ie=UTF8&t=h&source=embed&z=6&vpsrc=0&mid=\
+        1w_hd7cGhYzXqFqMB1VE41gtp5G0&ll=39.812755594880514%2C-3.916625999999951"
+    },
+
+    {
+      name: "croatia",
+      image:
+        "https://abouttravel.ch/wp-content/uploads/2017/08/17-290-10_Kroatien-696x385.jpg",
+      types: ["europa"],
+      aboutText:
+        "Kroatien (kroatisch Hrvatska), amtlich Republik Kroatien (kroatisch Republika Hrvatska), ist ein Staat in der Übergangszone zwischen Mittel- und Südosteuropa. Kroatien ist Mitglied der Europäischen Union, der NATO, der Welthandelsorganisation, der OSZE und der Vereinten Nationen. Entsprechend dem Index der menschlichen Entwicklung zählt Kroatien zu den sehr hoch entwickelten Staaten.\
+        Das Staatsgebiet liegt östlich des Adriatischen Meeres und zum Teil im Südwesten der Pannonischen Tiefebene. Im Nordwesten bildet Slowenien, im Norden Ungarn, im Nordosten Serbien, im Osten Bosnien und Herzegowina und im Südosten Montenegro die Grenze. Das Gebiet der einstigen Republik Ragusa (Dubrovačka Republika), das heute den südlichsten Teil des Staates ausmacht, verfügt über keine direkte Landverbindung zum übrigen Staatsgebiet, da der wenige Kilometer breite Meereszugang von Bosnien und Herzegowina dazwischen liegt; das Gebiet um",
+      link:
+        "https://www.google.com/maps/d/u/0/viewer?msa=0&ie=UTF8&t=m&source=\
+        embed&ll=42.700603999999984%2C17.94410700000003&spn=0.151386%2C0.205994&z=\
+        11&mid=1PqhGBngTDpdnamLZ25HzU91q2ks"
+    },
+
+    {
+      name: "kenia",
+      image:
+        "https://cdn.britannica.com/64/94364-050-9B783F3B/Sand-dunes-vegetation-Sossusvlei-Namibia-Namib-desert.jpg",
+      types: ["africa"],
+      aboutText:
+        "Kenia [ˈkʰeːni̯a] (Swahili, englisch Kenya [ˈkʰɛnjə, ˈkʰiːnjə]), offiziell die Republik Kenia, ist ein Staat in Ostafrika. Er grenzt im Nordwesten an den Südsudan, im Norden an Äthiopien, im Nordosten an Somalia, im Süden an Tansania, im Westen an Uganda und im Südosten an den Indischen Ozean. Die Hauptstadt und größte Stadt des Landes ist Nairobi, die zweite Millionenstadt ist Mombasa. Die Wirtschaft Kenias ist, gemessen am Bruttoinlandsprodukt, die größte in Südost- und Zentralafrika.",
+      link:
+        "https://www.google.com/maps?q=kenia&source=lmns&bih=686&biw=\
+        1330&rlz=1C5CHFA_enAT860AT860&hl=de&ved=\
+        2ahUKEwiopeWXgNfnAhUE7xoKHUHLDiQQ_AUoAXoECAEQAQ"
+    },
+
+    {
+      name: "madagaskar",
+      image:
+        "https://www.wwf.ch/sites/default/files/styles/page_cover_large_16_9/public/2017-02/Madagaskar-Baobab-B%C3%A4ume.jpg?itok=yU6sKg0w",
+      types: ["africa"],
+      aboutText:
+        "Madagaskar ist ein riesiger Inselstaat vor der afrikanischen Südostküste. Hier gibt es Tausende Tierarten wie Lemuren, die nur auf Madagaskar vorkommen, Regenwälder, Strände und Riffe. In der Nähe der geschäftigen Hauptstadt Antananarivo liegt Ambohimanga, ein Königshügel mit Palästen und Grabstätten, sowie die „Baobab-Allee“, eine von gewaltigen jahrhundertealten Affenbrotbäumen gesäumte unbefestigte Straße.",
+      link:
+        "https://www.google.com/maps/place/Madagaskar/@-18.7719242,42.3713071,6z/data=\
+        !3m1!4b1!4m5!3m4!1s0x21d1a4e3ea238545:0x5244e3c1977b1388!8m2!3d-18.766947!4d46.869107"
+    }
+  ]
+};
+
+const list = document.querySelector("#land-info ul");
+
+//results.data.forEach(land => {
+//setupListItem(land);
+//});
+
+function setupListItem(land) {
+  // const name = land.name;
+  // const image = land.image;
+  // ...
+
+  const { name, image, types, aboutText, link } = land;
+
+  const listItem = document.createElement("li");
+
+  // Build header
+
+  const placeNameHeader = document.createElement("span");
+  const placeNameHeaderContent = document.createTextNode(name);
+  placeNameHeader.appendChild(placeNameHeaderContent);
+  // attach headeer to list item
+  listItem.appendChild(placeNameHeader);
+
+  // build about section
+  const aboutTextEl = document.createElement("p");
+  aboutTextEl.className = "text";
+  aboutTextEl.style.display = "none";
+  const aboutTextElContent = document.createTextNode(aboutText);
+  aboutTextEl.appendChild(aboutTextElContent);
+  // attach aboutTextEl to list item
+  listItem.appendChild(aboutTextEl);
+
+  // build link
+  const a = document.createElement("a");
+  a.style.display = "none";
+  a.href = link;
+  a.target = "_blank";
+  const infoLinkContent = document.createTextNode(link);
+  a.appendChild(infoLinkContent);
+
+  // infoLink to parent 'P' --aboutTextEl
+  aboutTextEl.appendChild(a);
+
+  // build buttons
+  const infoBtn = document.createElement("button");
+  infoBtn.className = "info";
+  const infoBtnContent = document.createTextNode("Show more");
+  infoBtn.appendChild(infoBtnContent);
+
+  const hideInfoBtn = document.createElement("button");
+  hideInfoBtn.className = "no-info";
+  const hideInfoBtnContent = document.createTextNode("Show less");
+  hideInfoBtn.appendChild(hideInfoBtnContent);
+
+  // attach btns to list item
+  listItem.appendChild(infoBtn);
+  listItem.appendChild(hideInfoBtn);
+
+  // build image
+  const imageEl = document.createElement("img");
+  imageEl.src = image;
+  imageEl.style.width = "320px";
+  imageEl.style.height = "280px";
+  // ... TODO at home, append to the list item
+  listItem.appendChild(imageEl);
+
+  // add item to "land-info"--ul list
+  list.appendChild(listItem);
+}
+
+const chooseOfType = type => {
+  //first clear all the lands, then filter out each land
+  list.innerHTML = "";
+
+  results.data
+    .filter(land => land.types.includes(type))
+    .forEach(land => {
+      setupListItem(land);
+    });
+};
+
+const removeSelection = () => {
+  list.innerHTML = "";
+};
+
+//----------------------------Select Content--------------------------
+
+const selectionId = document.getElementById("selection");
+
+selectionId.addEventListener("change", function() {
+  if (selectionId.value !== "") {
+    chooseOfType(selectionId.value);
+  } else {
+    removeSelection();
+  }
+});
+
+//----------------------------slide images----------------------------
+
+const images = [
+  "shanghai.jpg",
+  "singapore.jpg",
+  "spain.jpg",
+  "madagaskar.jpg",
+  "germany.jpg"
+];
+
+num = 0;
+
+function next() {
+  let slider = document.getElementById("image");
+  num++;
+  if (num >= images.length) {
+    num = 0;
+  }
+  slider.src = images[num];
+}
+setInterval(next, 2000);
+
+//-------------------------JS----Enter event--------------------------
+
+document.querySelector("#enter").addEventListener("click", function() {
+  let enterText = document.querySelector("#enter");
+  let showSelect = document.getElementById("selection");
+  //console.log(typeof(showSelect));
+  if (enterText.innerHTML !== "Exit") {
+    showSelect.style.display = "inline";
+    showSelect.style.height = "2em";
+    showSelect.style.width = "10em";
+    enterText.innerHTML = "Exit";
+  } else {
+    showSelect.style.display = "none";
+    enterText.innerHTML = "Enter";
+    removeSelection();
+  }
+});
+//-------------------------JS---mouse event---------------------------
+
+/*document.querySelector("#land-info").addEventListener("click", function(e) {
+  let showText = document.getElementsByClassName("text");
+  //console.log(showText);
+  for (let i = 0; i < showText.length; i++) {
+    e.target.className == "info"
+      ? (showText[i].style.display = "block")
+      : (showText[i].style.display = "none");
+  }
+});*/
+
+//---------------------------JQuery Button MouseEvent------------------
+
+// "this" keyword refers to the parent scope. Arrow functions do not have their own scope, the "this" in them, refers to the scope above.
+$(document).ready(() => {
+  $("#land-info").on("click", ".info", function() {
+    const text = $("#land-info").find(".text");
+    const link = $("#land-info").find("a");
+
+    const letterSpace = "1px";
+    const fontWeight = "600";
+
+    text
+      .css({
+        letterSpace: letterSpace,
+        fontWeight: fontWeight
+      })
+      .fadeIn(2000);
+
+    link.fadeIn(3000);
+  });
+
+  $("#land-info").on("click", ".no-info", function() {
+    const text = $("#land-info").find(".text");
+    const link = $("#land-info").find("a");
+    text.fadeOut(1000);
+    link.fadeOut(1000);
+  });
+});
+
+//---------------------------JQuery Button MouseEvent------------------
+
+// "this" keyword refers to the parent scope. Arrow functions do not have their own scope, the "this" in them, refers to the scope above.
+/*$(document).ready(() => {
+  $("#main-kaffee").on("click", ".info", function() {
+    const text = $("#main-kaffee").find(".text");
+    const link = $("#main-kaffee").find(".link");
+
+    const letterSpace = "1px";
+    const fontWeight = "600";
+    text
+      .css({
+        letterSpace: letterSpace,
+        fontWeight: fontWeight
+      })
+      .fadeIn(2000);
+
+    link.fadeIn(3000);
+  });
+
+  $("#main-kaffee").on("click", ".no-info", function() {
+    const text = $("#main-kaffee").find(".text");
+    const link = $("#main-kaffee").find(".link");
+    text.fadeOut(1000);
+    link.fadeOut(1000);
+  });
+});*/
